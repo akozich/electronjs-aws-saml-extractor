c8cb7ead2db2df45fa59d51bb7f313a081e667f0
Aliaksei Kozich <a_kozich@flo.health>
2019-11-20 15:49:26 +0300

    Upgrade

--- browser.js
+++ browser.js
@@ -1,64 +1,63 @@
-const path = require('path')
-const glob = require('glob')
-const {app, BrowserWindow} = require('electron')
-const { session } = require('electron')
-const { webContents } = require('electron')
-const { parse } = require('qs')
-const url = require('./options').url
+const {app, BrowserWindow} = require('electron');
+const {session} = require('electron');
+const {parse} = require('qs');
+const {url} = require('./options');
 
 // Modify the user agent for all requests to the following urls.
 const filter = {
-  urls: ['https://signin.aws.amazon.com/saml']
-}
+	urls: ['https://signin.aws.amazon.com/saml']
+};
 
-// const debug = /--debug/.test(process.argv[2])
+// Const debug = /--debug/.test(process.argv[2])
 const debug = false;
 
-if (process.mas) app.setName('SSO Sign in')
-
-let mainWindow = null
-
-function initialize () {
-  return new Promise((resolve, reject) => {
-    function createWindow () {
-      const windowOptions = {
-        width: 400,
-        minWidth: 400,
-        height: 800,
-        title: app.getName()
-      }
-  
-      mainWindow = new BrowserWindow(windowOptions)
-      mainWindow.loadURL(url)
-  
-      // Launch fullscreen with DevTools open, usage: npm run debug
-      if (debug) {
-        mainWindow.webContents.openDevTools()
-        // mainWindow.maximize()
-        require('devtron').install()
-      }
-  
-      mainWindow.on('closed', () => {
-        mainWindow = null
-      })
-  
-      session.defaultSession.webRequest.onBeforeRequest(filter, (details, callback) => {
-        const contents = details.uploadData[0].bytes.toString();
-        const form_data = parse(contents);
-        callback({cancel: true});
-  
-        require('./assume-role').aquireShortLivedCredentials(form_data.SAMLResponse).then(resolve, reject)
-      })
-    }
-  
-    app.on('window-all-closed', () => {
-      if (process.platform !== 'darwin') {
-        app.quit()
-      }
-    })
-
-    createWindow();
-  });
+if (process.mas) {
+	app.setName('SSO Sign in');
 }
 
-module.exports = { initialize }
+let mainWindow = null;
+
+function initialize() {
+	return new Promise((resolve, reject) => {
+		function createWindow() {
+			const windowOptions = {
+				width: 400,
+				minWidth: 400,
+				height: 800,
+				title: app.getName()
+			};
+
+			mainWindow = new BrowserWindow(windowOptions);
+			mainWindow.loadURL(url);
+
+			// Launch fullscreen with DevTools open, usage: npm run debug
+			if (debug) {
+				mainWindow.webContents.openDevTools();
+				// MainWindow.maximize()
+				require('devtron').install();
+			}
+
+			mainWindow.on('closed', () => {
+				mainWindow = null;
+			});
+
+			session.defaultSession.webRequest.onBeforeRequest(filter, (details, callback) => {
+				const contents = details.uploadData[0].bytes.toString();
+				const formData = parse(contents);
+				callback({cancel: true});
+
+				require('./assume-role').aquireShortLivedCredentials(formData.SAMLResponse).then(resolve, reject);
+			});
+		}
+
+		app.on('window-all-closed', () => {
+			if (process.platform !== 'darwin') {
+				app.quit();
+			}
+		});
+
+		createWindow();
+	});
+}
+
+module.exports = {initialize};
--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const config = require('./config')
-const getCredentials = require('./browser').initialize
-const { app } = require('electron')
+const {app} = require('electron');
+const config = require('./config');
+const getCredentials = require('./browser').initialize;
 
 app.on('ready', () => {
-  config.loadCredentials()
-  .catch(()=> {})
-  .then(credentials => {
-    if (credentials && Date.parse(credentials.Expiration) > new Date()) {
-      return credentials;
-    } else {
-      return getCredentials();
-    }
-  })
-  .then(JSON.stringify)
-  .then(console.log)
-  .finally(app.quit)
+	config.loadCredentials()
+		.catch(() => {})
+		.then(credentials => {
+			if (credentials && Date.parse(credentials.Expiration) > new Date()) {
+				return credentials;
+			}
+
+			return getCredentials();
+		})
+		.then(JSON.stringify)
+		.then(console.log)
+		.finally(app.quit);
 });
